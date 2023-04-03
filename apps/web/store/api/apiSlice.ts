28769166a8e5fd500291ef22c6ea3ae309b5801e
Marcin Sacha <marcindev03@gmail.com>
2023-04-03 13:21:58 +0200

    Fix api slice typescript error

--- apps/web/store/api/apiSlice.ts
+++ apps/web/store/api/apiSlice.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { HYDRATE } from "next-redux-wrapper";
+import { RootState } from "store/store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:1337/api/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;
+    const token = (getState() as RootState).auth.token;
 
     if (token) {
       headers.set("Authorization", `Bearer ${token}`);
