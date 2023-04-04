80a79a9cac25d2f62c8b03bc8af209747963366c
Marcin Sacha <marcindev03@gmail.com>
2023-04-04 10:45:49 +0200

    Dynamic dashboard cards

--- apps/web/pages/admin/dashboard.tsx
+++ apps/web/pages/admin/dashboard.tsx
@@ -1,7 +1,3 @@
-import CardLineChart from "components/Cards/CardLineChart";
-import CardBarChart from "components/Cards/CardBarChart";
-import CardPageVisits from "components/Cards/CardPageVisits";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic";
 import Admin from "layouts/Admin";
 import Head from "next/head";
 import { wrapper } from "store";
@@ -11,6 +7,20 @@ import {
   getMostValuableCurrencies,
   getRunningQueriesThunk,
 } from "store/features/dashboard/dashboardApiSlice";
+import dynamic from "next/dynamic";
+
+const DynamicCardLineChart = dynamic(
+  () => import("components/Cards/CardLineChart")
+);
+const DynamicCardBarChart = dynamic(
+  () => import("components/Cards/CardBarChart")
+);
+const DynamicCardPageVisits = dynamic(
+  () => import("components/Cards/CardPageVisits")
+);
+const DynamicCardSocialTraffic = dynamic(
+  () => import("components/Cards/CardSocialTraffic")
+);
 
 export default function Dashboard() {
   return (
@@ -20,18 +30,18 @@ export default function Dashboard() {
       </Head>
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
+          <DynamicCardLineChart />
         </div>
         <div className="w-full xl:w-4/12 px-4">
-          <CardBarChart />
+          <DynamicCardBarChart />
         </div>
       </div>
       <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
+          <DynamicCardPageVisits />
         </div>
         <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
+          <DynamicCardSocialTraffic />
         </div>
       </div>
     </>
