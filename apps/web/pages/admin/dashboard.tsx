be566352ef15440f53eb6fc42e08e16e31f94322
Marcin Sacha <marcindev03@gmail.com>
2023-04-14 11:12:17 +0200

    Correct dashboard layout

--- apps/web/pages/admin/dashboard.tsx
+++ apps/web/pages/admin/dashboard.tsx
@@ -18,21 +18,11 @@ export default function Dashboard() {
       <Head>
         <title>Admin - Dashboard</title>
       </Head>
-      <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardBarChart />
-        </div>
-      </div>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
-        </div>
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-x-4 gap-y-6">
+        <CardLineChart />
+        <CardBarChart />
+        <CardPageVisits />
+        <CardSocialTraffic />
       </div>
     </>
   );
